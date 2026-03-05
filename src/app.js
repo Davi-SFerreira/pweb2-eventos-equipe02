3e03326b928227b5cc16b3213e0b85abc0231dc0
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-03-05 19:54:43 -0300

    feat: adicionando import do bd e criando as rotas /eventos e /eventos/:id

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from "express"
+import { EventosDatabase } from './database/EventosDatabase.js';
 
+const db = new EventosDatabase()
 const app = express()
 app.use(express.json())
 
+app.get("/eventos", (req, res) => {
+    const evento = db.listarTodos()
+    res.status(200).json(evento)
+})
 
+app.get("/eventos/:id", (req, res) => {
+    const { id } = req.params
+    const evento = db.buscarPorId(Number(id))
+
+    if(!evento){
+        res.status(404).json({mensagem: "Evento não encontrado"})
+        return
+    }
+    res.status(200).json({evento})
+})
 
 export default app
