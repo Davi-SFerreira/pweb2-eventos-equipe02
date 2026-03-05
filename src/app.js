564d815f05bf416fe07c848a025dea9a0c325536
Davi-SFerreira <dsf9@aluno.ifal.edu.br>
2026-03-05 20:14:45 -0300

    fix: Alterei as rotas do DELETE eventos, GET eventos se ele está ativo e GET eventos se sua modalidade é presencial.

--- src/app.js
+++ src/app.js
@@ -24,24 +24,28 @@ app.get("/eventos/:id", (req, res) => {
 app.delete("/eventos/:id", (req, res) => {
     const { id } = req.params
     const evento = db.buscarPorId(Number(id))
-
     if(!evento){
         res.status(404).json({mensagem: "Evento não encontrado"})
         return
     }
-
     db.remover(Number(id))
     res.status(204).send()
 })
 
-app.get("/eventos?ativo=true", (req, res) => {
-    const eventosAtivos = db.listarAtivos()
-    res.status(200).json(eventosAtivos)
+app.get("/eventos", (req, res) => {
+    const { ativo } = req.query
+    if(ativo === "true"){
+        const eventosAtivos = db.listarAtivos()
+        res.status(200).json(eventosAtivos)
+    }
 })
 
-app.get("/eventos?modalidade=presencial", (req, res) => {
-    const eventosPresenciais = db.listarPorModalidade("presencial")
-    res.status(200).json(eventosPresenciais)
+app.get("/eventos", (req, res) => {
+    const { modalidade } = req.query
+    if(modalidade === "presencial"){
+        const eventosPresenciais = db.listarPorModalidade("presencial")
+        res.status(200).json(eventosPresenciais)
+    }
 })
 
 export default app
